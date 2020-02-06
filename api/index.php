b42a1c45ad787559d16a6a3711f626e0aad5859e
kinba3000 <50628586+kinba3000@users.noreply.github.com>
2020-02-06 22:29:04 +0100

    Basic Routing created

--- api/index.php
+++ api/index.php
@@ -1,5 +1,32 @@
-<?php
+ <?php
+
+
+    function test($param1)
+    {
+        echo "Test erfolgreich";
+        if (isset($param1)) {echo $param1;};
+    };
 
 
 
-?>
+
+    $url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : [];
+
+    print_r($url);
+    var_dump($url);
+
+
+
+    if (function_exists($url[0])) {
+        echo "function  found";
+        if (isset($url[0])) {
+            $url[0]($url[1]);
+        };
+    } else {
+        echo "function not found";
+        header("HTTP/1.0 404 Not Found");
+    };
+
+    ?>
+
+ <p class="">This is the Index</p>
