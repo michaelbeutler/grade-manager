eb68ba9ab5fbcc716a706f7923621897f40277a9
kinba3000 <50628586+kinba3000@users.noreply.github.com>
2020-02-06 23:18:30 +0100

    addet autoload(not working jeet), calling funktions overurl.

--- api/index.php
+++ api/index.php
@@ -4,7 +4,9 @@
     function test($param1)
     {
         echo "Test erfolgreich";
-        if (isset($param1)) {echo $param1;};
+        if (isset($param1)) {
+            echo $param1;
+        };
     };
 
 
@@ -12,21 +14,29 @@
 
     $url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : [];
 
-    print_r($url);
+
     var_dump($url);
 
+    function check_routes($ary0)
+    {
+        spl_autoload_register(function ($ary0) {
+            include 'routes/' . $ary0 . '.php';
+        });
+    };
 
+if (check_routes($url[0])) {echo "Gefunden";} else {echo "nicht gefunden";};
 
-    if (function_exists($url[0])) {
+    if (!function_exists($url[0]) or !check_routes($url[0])) {
+        echo "function not found";
+        header("HTTP/1.0 404 Not Found");
+    } else {
+
         echo "function  found";
         if (isset($url[0])) {
             $url[0]($url[1]);
         };
-    } else {
-        echo "function not found";
-        header("HTTP/1.0 404 Not Found");
     };
-
+echo $globalvar;
     ?>
 
  <p class="">This is the Index</p>
